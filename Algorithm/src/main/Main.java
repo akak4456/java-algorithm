2e4a749a938abdda9dbd7bb6edc4cc860d55e319
akak35793-oss <akak4456@naver.com>
2026-03-09 18:33:09 +0900

    2739번 구구단

--- Algorithm/src/main/Main.java
+++ Algorithm/src/main/Main.java
@@ -11,10 +11,14 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
 
-public class Main { 
+public class Main {
+	private static int N;
 	public static void main(String[] args) throws Exception {
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 //		StringTokenizer st = new StringTokenizer(br.readLine());
-		System.out.println("Hello World!");
+		N = Integer.parseInt(br.readLine());
+		for(int i = 1;i<=9;i++) {
+			System.out.println(N + " * " + i + " = " + (N*i));
+		}
 	}
 }
