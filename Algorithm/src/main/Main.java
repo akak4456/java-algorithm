ccd8181903b77adab20ac263563b0dc568c5ee1a
akak35793-oss <akak4456@naver.com>
2026-03-09 17:40:25 +0900

    first commit

---
+++ Algorithm/src/main/Main.java
@@ -0,0 +1,25 @@
+package main;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main { 
+	private static String S;
+	private static int N;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringTokenizer st = new StringTokenizer(br.readLine());
+		S = br.readLine();
+		for(int i=0;i<S.length();i++) {
+			System.out.print(S.charAt(S.length() - i - 1));
+		}
+	}
+}
