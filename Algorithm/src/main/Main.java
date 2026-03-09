14fb498c282f3be91f1b5c1ee300197879b9e7d1
akak35793-oss <akak4456@naver.com>
2026-03-09 18:31:43 +0900

    2557번 Hello World

--- Algorithm/src/main/Main.java
+++ Algorithm/src/main/Main.java
@@ -12,15 +12,9 @@ import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main { 
-	private static int A, B, C, D, E;
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		A = Integer.parseInt(st.nextToken());
-		B = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		D = Integer.parseInt(st.nextToken());
-		E = Integer.parseInt(st.nextToken());
-		System.out.println((A * A + B * B + C * C + D * D + E * E)% 10);
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringTokenizer st = new StringTokenizer(br.readLine());
+		System.out.println("Hello World!");
 	}
 }
