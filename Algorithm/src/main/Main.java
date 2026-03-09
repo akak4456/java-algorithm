234d00709b98fc888b1d88543c869fcd002e9804
akak35793-oss <akak4456@naver.com>
2026-03-09 18:30:40 +0900

    2475번 검증수

--- Algorithm/src/main/Main.java
+++ Algorithm/src/main/Main.java
@@ -12,16 +12,15 @@ import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main { 
-	private static int N;
+	private static int A, B, C, D, E;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		// StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(br.readLine());
-		for(int i=1;i<=N;i++) {
-			for(int j=1;j<=i;j++) {
-				System.out.print("*");
-			}
-			System.out.println();
-		}
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		A = Integer.parseInt(st.nextToken());
+		B = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		D = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
+		System.out.println((A * A + B * B + C * C + D * D + E * E)% 10);
 	}
 }
