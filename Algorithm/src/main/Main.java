2e9afb44ff17a2629d36074a968ef744774f9d86
akak35793-oss <akak4456@naver.com>
2026-03-11 16:46:35 +0900

    10952번 A+B - 5

--- Algorithm/src/main/Main.java
+++ Algorithm/src/main/Main.java
@@ -1,18 +1,26 @@
 package main;
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 public class Main {
-	private static int T;
+	private static int A, B;
 	public static void main(String[] args) throws Exception {
-		Scanner in = new Scanner(System.in);
-		
-		while(in.hasNextInt()) {
-			int A = in.nextInt();
-			int B = in.nextInt();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while(true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			A = Integer.parseInt(st.nextToken());
+			B = Integer.parseInt(st.nextToken());
+			if(A == 0 && B == 0) break;
 			System.out.println(A + B);
 		}
-		
-		in.close();
-	}  
+	}
 }
