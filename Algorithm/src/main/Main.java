d41795f59319465ded3f2f48b8740d68e5af6654
akak35793-oss <akak4456@naver.com>
2026-03-11 16:47:45 +0900

    10998번 AxB

--- Algorithm/src/main/Main.java
+++ Algorithm/src/main/Main.java
@@ -15,12 +15,9 @@ public class Main {
 	private static int A, B;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		while(true) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			A = Integer.parseInt(st.nextToken());
-			B = Integer.parseInt(st.nextToken());
-			if(A == 0 && B == 0) break;
-			System.out.println(A + B);
-		}
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		A = Integer.parseInt(st.nextToken());
+		B = Integer.parseInt(st.nextToken());
+		System.out.println(A * B);
 	}
 }
