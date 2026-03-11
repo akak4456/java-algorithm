ef904f31c648c1b7a2f36b03818f1fb2453dfc61
akak35793-oss <akak4456@naver.com>
2026-03-11 16:45:16 +0900

    10951번 A+B - 4

--- Algorithm/src/main/Main.java
+++ Algorithm/src/main/Main.java
@@ -1,27 +1,18 @@
 package main;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 	private static int T;
-	private static int A, B;
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		T = Integer.parseInt(br.readLine());
-		for(int i=0;i<T;i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			A = Integer.parseInt(st.nextToken());
-			B = Integer.parseInt(st.nextToken());
+		Scanner in = new Scanner(System.in);
+		
+		while(in.hasNextInt()) {
+			int A = in.nextInt();
+			int B = in.nextInt();
 			System.out.println(A + B);
 		}
-	}
+		
+		in.close();
+	}  
 }
