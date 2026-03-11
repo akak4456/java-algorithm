e5609ac7a37496c3a1b9c446af63a0e24890f17a
akak35793-oss <akak4456@naver.com>
2026-03-11 16:37:39 +0900

    10171번 고양이

--- Algorithm/src/main/Main.java
+++ Algorithm/src/main/Main.java
@@ -14,19 +14,11 @@ import java.util.StringTokenizer;
 public class Main {
 	private static int N;
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 //		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(br.readLine());
-		if(90 <= N && N <= 100) {
-			System.out.println("A");
-		} else if(80 <= N && N <= 89) {
-			System.out.println("B");
-		} else if(70 <= N && N <= 79) {
-			System.out.println("C");
-		} else if(60 <= N && N <= 69) {
-			System.out.println("D");
-		} else {
-			System.out.println("F");
-		}
+		System.out.println("\\    /\\\r\n"
+				+ " )  ( ')\r\n"
+				+ "(  /  )\r\n"
+				+ " \\(__)|");
 	}
 }
