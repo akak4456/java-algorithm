bd5f20f3c80c1fa3d49ab9a86a7171314eb72744
akak35793-oss <akak4456@naver.com>
2026-03-13 17:25:22 +0900

    1152번 단어의 개수

--- Algorithm/src/main/Main.java
+++ Algorithm/src/main/Main.java
@@ -12,26 +12,9 @@ import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
-	private static int N;
-	private static int[] arr;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		arr = new int[N];
-		for(int i=0;i<N;i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		int minVal = 987654321;
-		int maxVal = -987654321;
-		for(int i=0;i<N;i++) {
-			if(arr[i] < minVal) {
-				minVal = arr[i];
-			}
-			if(arr[i] > maxVal) {
-				maxVal = arr[i];
-			}
-		}
-		System.out.println(minVal + " " + maxVal);
+		System.out.println(st.countTokens());
 	}
 }
