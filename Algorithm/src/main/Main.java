1caf54593a73a713557dac3f5ffd9db66aead4dc
akak35793-oss <akak4456@naver.com>
2026-03-13 17:06:49 +0900

    31403번 A+B-C

--- Algorithm/src/main/Main.java
+++ Algorithm/src/main/Main.java
@@ -12,19 +12,16 @@ import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
-	private static int N;
-	private static String s;
+	private static String A, B, C;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		// StringTokenizer st = new StringTokenizer(br.readLine());
 //		str = br.readLine();
 //		System.out.println((int)str.charAt(0));
-		N = Integer.parseInt(br.readLine());
-		s = br.readLine();
-		int sum = 0;
-		for(int i=0;i<N;i++) {
-			sum += s.charAt(i) - '0';
-		}
-		System.out.println(sum);
+		A = br.readLine();
+		B = br.readLine();
+		C = br.readLine();
+		System.out.println(Integer.parseInt(A) + Integer.parseInt(B) - Integer.parseInt(C));
+		System.out.println(Integer.parseInt(A + B) - Integer.parseInt(C));
 	}
 }
