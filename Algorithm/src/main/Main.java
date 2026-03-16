8d0d0589d0c9f88d4df35bcbac9b5c093c2f9ffa
akak35793-oss <akak4456@naver.com>
2026-03-16 16:15:50 +0900

    2577번 숫자의 개수

--- Algorithm/src/main/Main.java
+++ Algorithm/src/main/Main.java
@@ -12,9 +12,21 @@ import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
+	private static int A, B, C;
+	private static int[] arr = new int[10];
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		System.out.println(st.countTokens());
+//		StringTokenizer st = new StringTokenizer(br.readLine());
+		A = Integer.parseInt(br.readLine());
+		B = Integer.parseInt(br.readLine());
+		C = Integer.parseInt(br.readLine());
+		String str = String.valueOf(A * B * C);
+		
+		for(int i=0;i<str.length();i++) {
+			arr[str.charAt(i) - '0']++;
+		}
+		for(int i=0;i<10;i++) {
+			System.out.println(arr[i]);
+		}
 	}
 }
