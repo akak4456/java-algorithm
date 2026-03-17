4ad0201c11c31a99f2cdbf0c21e2aadd583810bb
akak35793-oss <akak4456@naver.com>
2026-03-17 16:20:08 +0900

    2798번 블랙잭

--- Algorithm/src/main/Main.java
+++ Algorithm/src/main/Main.java
@@ -12,17 +12,33 @@ import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
-	private static int N;
+	private static int N, M;
+	private static int[] arr;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-		int step = 1;
-		int last = 1;
-		while(last < N) {
-			last += 6 * step;
-			step++;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		System.out.println(step);
+		int minDist = 987654321;
+		int minSum = -1;
+		for(int i=0;i<N;i++) {
+			for(int j=0;j<N;j++) {
+				if(i == j) continue;
+				for(int k = 0; k < N; k++) {
+					if(i == k || j == k) continue;
+					int sum = arr[i] + arr[j] + arr[k];
+					if(sum <= M && M - sum < minDist) {
+						minDist =Math.abs(sum - M);
+						minSum = sum;
+					}
+				}
+			}
+		}
+		System.out.println(minSum);
 	}
 }
