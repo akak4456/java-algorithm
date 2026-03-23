3f756d4fc679488ba3f3c6f977cf363f737eae85
akak35793-oss <akak4456@naver.com>
2026-03-23 18:46:36 +0900

    1920번 수 찾기

--- Algorithm/src/main/Main.java
+++ Algorithm/src/main/Main.java
@@ -8,39 +8,33 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.StringTokenizer;
 
-class Point {
-	int x;
-	int y;
-	public Point(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
-}
 public class Main {
-	private static int N;
-	private static Point[] points;
+	private static int N,M;
+	private static Set<Integer> s;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		// StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(br.readLine());
-		points = new Point[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		s = new HashSet<Integer>();
 		for(int i=0;i<N;i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int x = Integer.parseInt(st.nextToken());
-			int y = Integer.parseInt(st.nextToken());
-			points[i] = new Point(x, y);
+			int a = Integer.parseInt(st.nextToken());
+			s.add(a);
 		}
-		Arrays.sort(points, (p1, p2) -> {
-			if(p1.y == p2.y) {
-				return p1.x - p2.x;
-			}
-			return p1.y - p2.y;
-		});
+		M = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
 		for(int i=0;i<N;i++) {
-			System.out.println(points[i].x + " " + points[i].y);
+			int a = Integer.parseInt(st.nextToken());
+			if(s.contains(a)) {
+				System.out.println(1);
+			} else {
+				System.out.println(0);
+			}
 		}
 	}
 }
