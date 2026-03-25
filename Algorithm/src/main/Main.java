5997d5029fa5001f14a7a765b84f79840e7bebe3
akak35793-oss <akak4456@naver.com>
2026-03-25 16:01:20 +0900

    10816번 숫자 카드 2

--- Algorithm/src/main/Main.java
+++ Algorithm/src/main/Main.java
@@ -17,25 +17,34 @@ import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
-	private static int K;
-	private static Stack<Integer> st;
+	private static int N;
+	private static Map<Integer, Integer> m;
+	private static int M;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		// StringTokenizer st = new StringTokenizer(br.readLine());
-		K = Integer.parseInt(br.readLine());
-		st = new Stack<>();
-		for(int i=0;i<K;i++) {
-			int a = Integer.parseInt(br.readLine());
-			if(a == 0) {
-				st.pop();
+		N = Integer.parseInt(br.readLine());
+		m = new HashMap<>();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++) {
+			int a = Integer.parseInt(st.nextToken());
+			if(m.containsKey(a)) {
+				m.put(a, m.get(a) + 1);
 			} else {
-				st.add(a);
+				m.put(a, 1);
 			}
 		}
-		int sum = 0;
-		while(!st.isEmpty()) {
-			sum += st.pop();
+		StringBuilder sb = new StringBuilder();
+		M = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<M;i++) {
+			int a = Integer.parseInt(st.nextToken());
+			if(m.containsKey(a)) {
+				sb.append(m.get(a)).append(" ");
+			} else {
+				sb.append("0 ");
+			}
 		}
-		System.out.println(sum);
+		System.out.println(sb);
 	}
 }
