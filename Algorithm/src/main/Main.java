3f9d51419c801323dc3af9f8583aad40c4084736
akak35793-oss <akak4456@naver.com>
2026-03-26 17:55:22 +0900

    2108번 통계학

--- Algorithm/src/main/Main.java
+++ Algorithm/src/main/Main.java
@@ -16,33 +16,62 @@ import java.util.Queue;
 import java.util.Set;
 import java.util.Stack;
 import java.util.StringTokenizer;
+class Pair implements Comparable<Pair>{
+	int first;
+	int last;
+	public Pair(int first, int last) {
+		this.first = first;
+		this.last = last;
+	}
+	@Override
+	public int compareTo(Pair o) {
+		if(o.last == this.last) {
+			return this.first - o.first;
+		}
+		return o.last - this.last;
+	}
+}
 public class Main {
 	private static int N;
-	private static Stack<Integer> st;
-	private static StringBuilder sb;
+	private static int[] arr;
+	private static ArrayList<Pair> most;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		// StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(br.readLine());
-		st = new Stack<>();
-		sb = new StringBuilder();
-		int n = 1;
+		arr = new int[N];
 		for(int i=0;i<N;i++) {
-			int a = Integer.parseInt(br.readLine());
-			while(n <= a) {
-				st.add(n);
-				sb.append("+\n");
-				n++;
-			}
-			if(st.peek() == a) {
-				st.pop();
-				sb.append("-\n");
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(arr);
+		double sum = 0;
+		for(int i=0;i<N;i++) {
+			sum += arr[i];
+		}
+		System.out.println((int)Math.round(sum / N));
+		System.out.println(arr[N/2]);
+		int cnt = 1;
+		int val = arr[0];
+		most = new ArrayList<>();
+		for(int i=1;i<N;i++) {
+			if(val != arr[i]) {
+				most.add(new Pair(val, cnt));
+				cnt = 1;
+				val = arr[i];
+			} else {
+				cnt++;
 			}
 		}
-		if(st.isEmpty()) {
-			System.out.println(sb);
+		most.add(new Pair(val, cnt));
+		Collections.sort(most);
+//		for(int i=0;i<most.size();i++) {
+//			System.out.println(most.get(i).first + " " + most.get(i).last);
+//		}
+		if(most.size() >= 2 && most.get(0).last == most.get(1).last) {
+			System.out.println(most.get(1).first);
 		} else {
-			System.out.println("NO");
+			System.out.println(most.get(0).first);
 		}
+		System.out.println(arr[N-1] - arr[0]);
 	}
 }
