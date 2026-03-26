cf647168115752ae68023f7a842e0281023fe7be
akak35793-oss <akak4456@naver.com>
2026-03-26 17:21:41 +0900

    1929번 소수 구하기

--- Algorithm/src/main/Main.java
+++ Algorithm/src/main/Main.java
@@ -18,87 +18,32 @@ import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
-	private static int N, M;
-	private static char[][] board;
-	private static int getCnt(int startRow, int startCol) {
-		int result = 987654321;
-		int cnt = 0;
-		for(int i=0;i<8;i++) {
-			for(int j=0;j<8;j++) {
-				if(i%2 == 0) {
-					if(j % 2 == 0) {
-						if(board[startRow + i][startCol + j] != 'W') {
-							cnt++;
-						}
-					} else {
-						if(board[startRow + i][startCol + j] != 'B') {
-							cnt++;
-						}
-					}
-				} else {
-					if(j % 2 != 0) {
-						if(board[startRow + i][startCol + j] != 'W') {
-							cnt++;
-						}
-					} else {
-						if(board[startRow + i][startCol + j] != 'B') {
-							cnt++;
-						}
-					}
-				}
-			}
-		}
-		result = Math.min(result, cnt);
-		cnt = 0;
-		for(int i=0;i<8;i++) {
-			for(int j=0;j<8;j++) {
-				if(i%2 == 0) {
-					if(j % 2 != 0) {
-						if(board[startRow + i][startCol + j] != 'W') {
-							cnt++;
-						}
-					} else {
-						if(board[startRow + i][startCol + j] != 'B') {
-							cnt++;
-						}
-					}
-				} else {
-					if(j % 2 == 0) {
-						if(board[startRow + i][startCol + j] != 'W') {
-							cnt++;
-						}
-					} else {
-						if(board[startRow + i][startCol + j] != 'B') {
-							cnt++;
-						}
-					}
-				}
-			}
-		}
-		result = Math.min(result, cnt);
-		return result;
-	}
+	private static int M, N;
+	private static boolean[] isPrime;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		board = new char[N][M];
-		for(int i=0;i<N;i++) {
-			String line = br.readLine();
-			for(int j=0;j<M;j++) {
-				board[i][j] = line.charAt(j);
-			}
+		N = Integer.parseInt(st.nextToken());
+		isPrime = new boolean[1000000 + 1];
+		for(int i = 1;i<=1000000;i++) {
+			isPrime[i] = true;
 		}
-		int result = 987654321;
-		for(int row=0;row+8<=N;row++) {
-			for(int col=0;col+8<=M;col++) {
-				int cnt = getCnt(row, col);
-				if(cnt < result) {
-					result = cnt;
+		isPrime[0] = false;
+		isPrime[1] = false;
+		for(int i=2;i<=1000000;i++) {
+			if(isPrime[i]) {
+				int n = i + i;
+				while(n <= 1000000) {
+					isPrime[n] = false;
+					n += i;
 				}
 			}
 		}
-		System.out.println(result);
+		for(int i=M;i<=N;i++) {
+			if(isPrime[i]) {
+				System.out.println(i);
+			}
+		}
 	}
 }
